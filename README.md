c6668172a169401bb540450efb5fbbf09d4f2668
TexyTurvy <160274121+TexyTurvy@users.noreply.github.com>
2024-05-02 15:03:54 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tonton
 The repository for all my open source HackMUD scripts
+
+Useage:
+
+tonton.scry {t:#s.corp.public }
+Replace corp.public with the target you want to scan, at time of commit I am only tier 1 so my scripts are limited to 1100 characters, so functionality is a bit limited.
---
+++ scry.js
@@ -0,0 +1,79 @@
+function (context, args){ // t:#s.aon.public
+    
+	var t = args.t;
+	
+	
+	var gl = #fs.scripts.get_level({name:t.name});
+	
+	if(gl != 4)
+	{
+		return {ok:false, msg:"`Spell countered, target insecure`"}
+	}
+	
+	var r = t.call();
+	var r2 = r;
+	var m1 = r.match(/(\w+) \| (\w+)/);
+	
+    r = t.call({});
+	var m2 = r.match(/ith (\w+):"(\w+)"$/);
+	
+	var z = "";
+	if(m1 && m2)
+	{
+		var o = {};
+		o[m2[1]] = m1[2];
+		r = t.call(o);
+		var m3 = r.match(/tegy (\w+)/);
+		
+		
+		
+		if(m3)
+		{
+			var pkl = ["p", "pass", "password"];
+			for(var pk in pkl)
+			{
+				o[m2[1]] = m1[2];
+				o[pkl[pk]] = m3[1]
+				r = t.call(o);
+				if(r.indexOf('Authenticated') > -1)
+					break;
+			}
+			
+			o[m2[1]] = m1[1];
+			r = t.call(o);
+			var pm = "";
+			var um = "";
+			for (var fp in r)
+			{
+				var m4 = r[fp].match(/([a-zA-Z][\w]+\.[a-zA-Z][\w]+)/);//([\w.]+)/);
+				if(m4)
+				{
+					o[m2[1]] = m2[2];
+					o['project'] = m4[1];
+					r2 = t.call(o);
+					//pm = pm + m4[1] + " `1Res`: " +(typeof r2)+ "\n"
+					for(var fs in r2)
+					{
+						var m5 = r2[fs].match(/^([\w]+\.[\w]+)$/);
+						if(m5)
+						{
+							um = um + r2[fs] + "\n"
+						}
+					}
+				}
+			}
+			if(!um){ return {ok:false, msg:"Interference blocks your way, try again"}}
+			else{
+			//const [payToken, message] = #fs.wiz.spellbound({ action: "prepare", owner:"tonton", cost:1 }) // I decided that a scraper tool wasn't useful enough to charge for, even on successful runs
+			//	if(message) return message; 
+				// do action and confirm 
+			//	#fs.wiz.spellbound({ action: "charge", token: payToken })
+			z = "`2Your scrying has revealed the following enemies`:\n\n" + um
+			}
+		}
+		return z
+	} else
+	{
+		return {ok:false, msg:"The corruption blocks your attempts, try again."}
+	}   
+}
